dd9bddddef5afb03e92ce99fc9c066e50279b3ad
Rodrigo Escobar <rodrigo.escobar.z@usach.cl>
2020-12-28 21:21:24 -0300

    Creacion menu con problemas en input

--- lab3_stack_20244554_EscobarZamorano/src/main/java/lab3_stack_20244554_EscobarZamorano/Main.java
+++ lab3_stack_20244554_EscobarZamorano/src/main/java/lab3_stack_20244554_EscobarZamorano/Main.java
@@ -1,7 +1,10 @@
 package lab3_stack_20244554_EscobarZamorano;
 
-public class Main {
+//import java.util.Scanner;
 
+public class Main {
+    //public static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
         //Usuario usuario1 = new Usuario("pepe","xd",1000);
         
@@ -35,6 +38,49 @@ public class Main {
         //String nombre = usuario1.getUsername();
         
         //System.out.println(nombre);
+        
+        //------* MENU *------
+        //Se define una variable que va a guardar la opcion seleccionada por el usuario
+        int opcion;
+        
+        //Se muestra por pantalla el menu de opciones que puede elegir el usuario
+        System.out.println("\n###### STACK OVERFLOW ######\nRegistrado como: \n\nEscoja su opcion: ");
+        System.out.println("\n1. Agregar una pregunta\n2. Responder una pregunta\n3. Dar Recompensa\n4. Aceptar Respuesta\n5. Cerrar Sesion\n6. Salir del programa");
+        System.out.println("INTRODUZCA SU OPCION: ");
+        
+        //Se recibe la opcion ingresada por el usuario
+        //opcion = Integer.parseInt(scanner.nextLine());
+        
+        //Dependiendo la opcion que ingrese el usuario se procede a ejecutar el comando
+        /*
+        switch(opcion){
+            case 1:
+                System.out.println("Ha seleccionado Agregar una pregunta");
+                break;
+                
+            case 2:
+                System.out.println("Ha seleccionado Responder una pregunta");
+                break;
+                
+            case 3:
+                System.out.println("Ha seleccionado Dar Recompensa");
+                break;
+                
+            case 4:
+                System.out.println("Ha seleccionado Aceptar Respuesta");
+                break;
+                
+            case 5:
+                System.out.println("Ha seleccionado Cerrar Sesion");
+                break;
+                
+            case 6:
+                System.out.println("Ha seleccionado Salir del programa");
+                break;
+            
+            default:
+                System.out.println("Ha ingresado una opcion incorrecta");
+        }
+        */ 
     }
-    
 }
